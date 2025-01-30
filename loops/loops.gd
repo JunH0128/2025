b7d6cc6f8550b363a9789d76de2612d46e13a51b
JunH0128 <C24519656@myTUDublin.ie>
2025-01-30 21:45:05 +0000

    drawed a line

--- loops/loops.gd
+++ loops/loops.gd
@@ -13,5 +13,12 @@ func _draw() -> void:
 	draw_line(Vector2(50, 50), Vector2(200, 200), Color.WHITE)
 	draw_circle(Vector2(500, 500), 100 , Color.BLUE_VIOLET, true)
 	
+	var v = get_viewport_rect()
+	
+	draw_line(Vector2(0, v.size.y), Vector2(v.size.x,0), Color.ALICE_BLUE)
+	
+	for i in range(10):
+		print(i)
+	
 	
 	pass
