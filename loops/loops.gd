eb9cb13ddafadf935da0b55d67c7aeee1349fcc2
JunH0128 <C24519656@myTUDublin.ie>
2025-01-30 21:36:03 +0000

    updated loops.gd

--- loops/loops.gd
+++ loops/loops.gd
@@ -10,4 +10,5 @@ func _process(delta: float) -> void:
 func _draw() -> void:
 	var r = Rect2(10, 10, 100, 30)
 	draw_rect(r,Color.AQUA, false, 5)
+	draw_line(Vector2(50, 50), Vector2(200, 200), Color.WHITE)
 	pass
