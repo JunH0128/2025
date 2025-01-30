f692de5ace47ab6499440a65252c644c84771cca
JunH0128 <C24519656@myTUDublin.ie>
2025-01-30 22:02:38 +0000

    drawn a line in a loop

--- loops/loops.gd
+++ loops/loops.gd
@@ -31,7 +31,12 @@ func _draw() -> void:
 	for i in range(basket.size()):
 		print(basket[i])
 		
+	var num_lines = 10
+	var s = get_viewport_rect().size
+	var w = s.x / num_lines
 	
+	for i in range(num_lines):
+		draw_line(Vector2(i * w, 0),Vector2(i * w, s.y), Color.RED)
 	
 	
 	pass
